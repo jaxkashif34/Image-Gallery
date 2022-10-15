42f5d4f3b33089c5ac665b7e9e72cf66a784ac5e
Kashif Ali <jaxksahif34@gmail.com>
2022-10-15 23:09:16 +0500

    fix the env path

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
+dotenv.config();
 const express = require('express');
 const { allowedFiles, upload } = require('./server/middleware');
 const cors = require('cors');
--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
-dotenv.config({path:"../.env"});
+dotenv.config();
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
