0d60201c12c3ea675db565cce15b8276f4b080fe
Kashif Ali <jaxksahif34@gmail.com>
2022-10-16 10:38:52 +0500

    remove sensitive env variables

--- server/config/index.js
+++ server/config/index.js
@@ -2,8 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'di2zelkg2',
-  api_key: process.env.CLOUDINARY_API_KEY || 319113269968527,
-  api_secret: process.env.CLOUDINARY_API_SECRET || 'NTDs6BPvIg-QFV08WB2tZ_YIcfA',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 module.exports = { cloudinary };
